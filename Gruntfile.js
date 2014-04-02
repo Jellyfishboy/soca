69c6e59ecb8c4f75ce4459491328301f51920e8e
Billy Dallimore <billy@sngtrkr.com>
2014-04-02 11:52:34 +0100

    Finished setting up the grunt server configuration. Using compass for the CSS compilation. Installed and added modernizr & normalize-css to Bower.

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        watch: {
+            options: {
+                nospawn: true
+            },
+            compass: {
+                files: ['app/src/sass/{,*/}*.{scss,sass}'],
+                tasks: ['compass:server']
+            },
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
+                files: [
+                    'app/*.html',
+                    'app/css/{,*/}*.css',
+                    'app/src/sass/{,*/}*.{scss,sass}',
+                    'app/js/{,*/}*.js',
+                    'app/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, 'app'),
+                            lrSnippet
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        compass: {
+            options: {
+                sassDir: 'app/src/sass',
+                cssDir: 'app/css'
+            },
+            dist: {},
+            server: {
+                options: {
+                    debugInfo: true
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.registerTask('server', function (target) {
+
+        grunt.task.run([
+            'compass:server',
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
+};
