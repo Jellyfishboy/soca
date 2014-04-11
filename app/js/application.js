0e07e950cd4f8c9d1a4b4e6922f07e5f02b38a90
Billy Dallimore <billy@sngtrkr.com>
2014-04-11 13:57:42 +0100

    Finished product administration form. Modified bootstrap-tagsinput resource to be retrieveable via Bower. Introduced new attachment UI and default image selection. SKU form is now integrated with the SKU table.

--- app/js/application.js
+++ app/js/application.js
@@ -1,9 +1,26 @@
 (function() {
   $(document).ready(function() {
-    return $(".user-menu ul li:first-child a").hover((function() {
+    $(".user-menu ul li:first-child a").hover((function() {
       return $(".user-menu .fa-caret-up").css('color', '#2f363d');
     }), function() {
       return $(".user-menu .fa-caret-up").css('color', '#ffffff');
+    });
+    $('.new-file').click(function() {
+      return $(this).next('input[type="file"]').trigger('click');
+    });
+    $('.file-upload').change(function() {
+      var clean, parent, value;
+      value = $(this).val();
+      clean = value.replace(/^.*[\\\/]/, '');
+      parent = $(this).prev('.new-file');
+      parent.children('div').text(clean);
+      parent.css('background-color', '#8DC73F');
+      return parent.children('.icon-upload-3').css('top', '20px');
+    });
+    return $('.current-file').click(function() {
+      $(this).next('input[type="radio"]').prop('checked', true);
+      $('.current-file').removeClass('default');
+      return $(this).addClass('default');
     });
   });
 
--- app/src/coffee/application.coffee
+++ app/src/coffee/application.coffee
@@ -1,5 +1,24 @@
 $(document).ready ->
+
+    ## Handle colour change for user-menu caveat
     $(".user-menu ul li:first-child a").hover (->
         $(".user-menu .fa-caret-up").css 'color', '#2f363d'
     ), ->
         $(".user-menu .fa-caret-up").css 'color', '#ffffff'
+
+    ## Attachment upload UI
+    $('.new-file').click ->
+        $(@).next('input[type="file"]').trigger('click');
+
+    $('.file-upload').change ->
+        value = $(@).val()
+        clean = value.replace(/^.*[\\\/]/, '')
+        parent = $(@).prev('.new-file')
+        parent.children('div').text clean
+        parent.css 'background-color', '#8DC73F'
+        parent.children('.icon-upload-3').css 'top', '20px'
+        
+    $('.current-file').click ->
+        $(@).next('input[type="radio"]').prop 'checked', true
+        $('.current-file').removeClass 'default'
+        $(@).addClass 'default'
