f4a30ff55513dfbfc989b7b206876e4c0d12c0b1
Tom Dallimore <me@tomdallimore.com>
2016-06-10 15:44:00 +0700

    Added filters for order.

--- app/js/lib/soca.filter.js
+++ app/js/lib/soca.filter.js
@@ -29,5 +29,14 @@ soca.filter =
             item = $(this).find(':selected').data('sort');
             return $('.mixitup').mixItUp('sort', item);
         });
+    },
+    orders: function()
+    {
+        $('#filter-order').on('change', function() 
+        {
+            var item;
+            item = $(this).find(':selected').data('filter');
+            return $('.mixitup').mixItUp('filter', item);
+        });
     }
 }
